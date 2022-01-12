78e42e4861c4b8a29b92338f3d76e7ed0769abed
Sneha Agrawal <snehagrawal98@gmail.com>
2022-01-12 16:10:58 +0530

    Initial Commit

---
+++ GastosProvider/GastosProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GastosProviderApp.swift
+//  GastosProvider
+//
+//  Created by mnameit on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GastosProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
